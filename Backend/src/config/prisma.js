53a3f8962c78f14f65e241faf0f1b2fd3e2a1a62
KatDev <anousai.dev@gmail.com>
2026-06-06 20:15:19 +0700

    feat(auth): add new system register and login has been security ,and fix bug post api to catch error chore(lib) : new lib bcryptjs and jsonwebtoken feat(auth): register has new function security is have hash password and create user input database and check duplicate user id in database feat(auth) : login has new function security is have check userId in database and check password by compare hash password  , create payload for pull method db and create token key 1d for login website fix(catch err) : post api err becauce config prisma is not yet complete ,fix import  to be require, feat pull and check by env ,and refactor host , port , user , password , database ,to match env

--- Backend/src/config/prisma.js
+++ Backend/src/config/prisma.js
@@ -1,11 +1,24 @@
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "@prisma/client";
+const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
+const { PrismaClient } = require("../generated/prisma");
+require("dotenv").config();
+
+
+const dbUrl = process.env.DATABASE_URL;
+if (!dbUrl) {
+  throw new Error("DATABASE_URL is not defined in environment variables");
+}
+
+const url = new URL(dbUrl);
+
 
 const adapter = new PrismaMariaDb({
-  host: "localhost",
-  port: 3306,
+  host: url.hostname,
+  port: url.port,
+  user:url.username,
+  password:url.password,
+  database:url.pathname.substring(1),
   connectionLimit: 5,
 });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({adapter});
 
-module.exports = { prisma };
+module.exports =  prisma ;
--- Backend/src/controllers/auth.js
+++ Backend/src/controllers/auth.js
@@ -1,10 +1,38 @@
-
+const prisma = require('../config/prisma');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Register a new user
 exports.register = async(req, res) => {
     try{
-        console.log(req.body);
-        res.send('OK');
+        const {name , email , password } = req.body;
+        // validate the input
+        if(!name || !email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+
+        // Check Email is Exsist 
+        const user = await prisma.user.findUnique({
+            where : { email : email }
+        })
+        if(user){
+            return res.status(400).json({message:"Email is already exist"}); // อีเมลซ้ำ หรือ มีแล้ว
+        }
+            // Hash Password
+            const hashedPassword = await bcrypt.hash(String(password), 10);
+            console.log("hashedPassword", hashedPassword);
+
+            // Create User 
+            await prisma.user.create({
+                data:{
+                    name,
+                    email,
+                    password:hashedPassword
+                }
+            })
+
+        res.json({message:"User created successfully"});
+
     }catch(error){
         console.log(error);
         res.status(500).json({message:"Internal server error"});
@@ -15,12 +43,34 @@ exports.register = async(req, res) => {
 
 };
 
-
 // Login user
 exports.login = async(req, res) => {
   try{
-    console.log(req.body);
-    res.send('OK');
+    const {email, password} = req.body;
+    // Check Email is Exsist
+    const user = await prisma.user.findFirst({
+      where : { email : email }
+    })
+    if(!user || !user.enabled) return res.status(400).json({message:"User not found or not enabled"})
+    // Check Password is Correct 
+    const isPasswordMatch = await bcrypt.compare(String(password), user.password)
+    if(!isPasswordMatch) return res.status(400).json({message:"Invalid password"})
+    
+    
+    // Create payload
+    const payload ={
+      id:user.id,
+      email:user.email,
+      role:user.role
+    }
+    // Create token
+    jwt.sign(payload, process.env.JWT_SECRET,{expiresIn: "1d"}, (err,token)=>{
+      if(err){
+        return res.status(500).json({message: "Error signing token"})
+      }
+
+      res.json({payload,token});
+    })
   }catch(error){
     console.log(error);
     res.status(500).json({message:"Internal server error"});
