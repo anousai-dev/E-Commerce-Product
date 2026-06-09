134490e52a024b629b0ac51e371613f1a50b6552
KatDev <anousai.dev@gmail.com>
2026-06-09 11:24:27 +0700

    feat(category): implement list, create with ICT timezone, and remove category functions using prisma - change import from services to prisma config - list: fetch and return all categories from database - create: create category with name and set createdAt/updatedAt with ICT (UTC+7) timezone - remove: delete category by ID and reset AUTO_INCREMENT to 1

--- Backend/src/controllers/category.js
+++ Backend/src/controllers/category.js
@@ -1,12 +1,13 @@
-const router = require("../services/category");
+const prisma = require("../config/prisma");
+
 
 
 
 
 exports.list = async(req, res) => {
-    
     try{
-        res.json({message:"All categories"});
+        const category = await prisma.category.findMany();
+        res.json(category);
     }catch(error){
         console.log(error);
         res.status(500).json({message:"Internal server error"});
@@ -18,7 +19,18 @@ exports.list = async(req, res) => {
 
 exports.create = async(req, res) => {
    try{
-        res.json({message:"Create category"});
+        const {name} = req.body;
+        // ปรับเวลาให้เป็นเวลา ICT (UTC+7)
+        const localTime = new Date(new Date().getTime() + 7 * 60 * 60 * 1000);
+        const category = await prisma.category.create({
+            data:{
+                name:name,
+                createdAt: localTime,
+                updatedAt: localTime
+            }
+        })
+
+        res.json({message:"Create category Success !!"});
    }catch(error){
     console.log(error);
     res.status(500).json({message:"Internal server error"});
@@ -30,9 +42,18 @@ exports.create = async(req, res) => {
 
 exports.remove = async(req, res) => {
    try{
-        
+        const {id} = req.params;
+
+        const category = await prisma.category.delete({
+            where:{
+                id: Number(id)
+            }
+        })
+        // รีเซ็ต AUTO_INCREMENT ให้รันต่อจาก ID ล่าสุดที่มีอยู่ (หรือเริ่มที่ 1 หากไม่มีข้อมูล)
+        await prisma.$executeRawUnsafe(`ALTER TABLE Category AUTO_INCREMENT = 1`);
+
         // console.log(req.params);
-        res.json({message:"Remove category"});
+        res.json({message:"Remove category Success !" , category});
    }catch(error){
     console.log(error);
     res.status(500).json({message:"Internal server error"});
